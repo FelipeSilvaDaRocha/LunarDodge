bf3a9fb272b62101f4e57ca4ce7a1a216c23f77a
Felipe Rocha <felipesilvarocha01@gmail.com>
2025-03-30 12:39:48 -0300

    Creating the menu option and background Adding music from menu Adding asset

---
+++ code/Const.py
@@ -0,0 +1,12 @@
+# C
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 242, 0)
+
+#  M
+MENU_OPTION = ('START NOW',
+               'SCORE',
+               'EXIT')
+
+#  W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
